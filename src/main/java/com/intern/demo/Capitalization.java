b019e7ce0df0506b5779291484be0ac02ac55cdd
jessen <jessenicholas.t@gmail.com>
2021-03-08 15:41:15 +0700

    cleaned up imports on all files

--- src/main/java/com/intern/demo/Capitalization.java
+++ src/main/java/com/intern/demo/Capitalization.java
@@ -1,7 +1,6 @@
 package com.intern.demo;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.text.WordUtils;
 
 import java.util.List;
--- src/main/java/com/intern/demo/ImportedBook.java
+++ src/main/java/com/intern/demo/ImportedBook.java
@@ -1,6 +1,9 @@
 package com.intern.demo;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
